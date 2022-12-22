30f86784f1a810c8e56d6d58b0f45c6cb33d3b12
Soll <soll.sterben@gmail.com>
2022-12-23 00:39:30 +0500

    2

---
+++ src/main/java/bronebot/Bot.java
@@ -0,0 +1,44 @@
+package bronebot;
+
+import lombok.Getter;
+import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class Bot extends TelegramLongPollingCommandBot {
+
+    private final String BOT_NAME;
+    private final String BOT_TOKEN;
+
+    public Bot(String botName, String botToken) {
+        super();
+        this.BOT_NAME = botName;
+        this.BOT_TOKEN = botToken;
+    }
+    @Override
+    public String getBotUsername() {
+        return BOT_NAME;
+    }
+
+    @Override
+    public void processNonCommandUpdate(Update update) {
+
+    }
+
+    @Override
+    public String getBotToken() {
+        return BOT_TOKEN;
+    }
+
+    @Override
+    public void onRegister() {
+        super.onRegister();
+    }
+
+    @Override
+    public void onUpdatesReceived(List<Update> updates) {
+        super.onUpdatesReceived(updates);
+    }
+}
--- src/main/java/bronebot/BroneBotApplication.java
+++ src/main/java/bronebot/BroneBotApplication.java
@@ -1,4 +1,22 @@
 package bronebot;
 
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.util.Map;
+
 public class BroneBotApplication {
+
+    private static final Map<String, String> getenv = System.getenv();
+
+    public static void main(String[] args) {
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            //botsApi.registerBot(new Bot(getenv.get("BOT_NAME"), getenv.get("BOT_TOKEN")));
+            botsApi.registerBot(new Bot("sber_brone_bot", "5988847234:AAEmuoDsSPKwNv6jmIdCRFWcCMI0HO3VaGs"));
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
 }
